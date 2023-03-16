20dfbb268461a1992b91b010b7f81fde24b01a5d
Ssurryaa <desuryaa1@gmail.com>
2023-03-16 20:25:20 +0800

    up

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -337,61 +337,11 @@
 
     <!-- Leaflet -->
     <script>
+      var map = L.map('map').setView([-8.537304773847266, 115.12583771558118], 13)
 
-      var peta1 = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-      	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-      		'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-      		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      	id: 'mapbox/streets-v11'
-      });
-
-      var peta2 = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox/satellite-v9'
-      });
-
-
-      var peta3 = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      });
-
-      var peta4 = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox/dark-v10'
-      });
-
-      //var map = L.map('map').setView([-8.537304773847266, 115.12583771558118], 13)
-      var map = L.map('map', {
-        center: [-8.537304773847266, 115.12583771558118],
-        zoom: 13,
-        layers: [peta1]
-      });
-      
-      if (navigator.geolocation) {
-      const location = navigator.geolocation.getCurrentPosition(
-        (position) => {
-          map.setView(
-            [position.coords.latitude, position.coords.longitude],
-            13
-          )
-        }
-      )} else {
-        alert('Geolocation is not supported by this browser.')
-      }
-
-      var baseMaps = {
-        "Grayscale": peta1,
-        "Satellite": peta2,
-        "Streets": peta3,
-        "Dark": peta4,
-      };
-
-      L.control.layers(baseMaps).addTo(map);
-      map.attributionControl.setPrefix(false);
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: "&copy; GIS",
+      }).addTo(map);
 
       var markerIcon = L.icon({
         iconUrl: "map-marker.png",
